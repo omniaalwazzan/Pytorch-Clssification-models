0bf008724c905c605f43044326882d0f62e76091
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2023-02-07 16:08:23 +0000

    Create Custom_VGG.py

---
+++ Custom_VGG.py
@@ -0,0 +1,126 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import _LRScheduler
+import torch.utils.data as data
+
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torchsummary import summary 
+from sklearn import decomposition,manifold
+from sklearn import manifold
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from tqdm.notebook import tqdm, trange
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.model_zoo import load_url as load_state_dict_from_url
+
+import copy
+import random
+import time
+
+SEED = 1234
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+
+class VGG(nn.Module):
+    def __init__(self, features, output_dim):
+        super().__init__()
+
+        self.features = features
+
+        self.avgpool = nn.AdaptiveAvgPool2d(7)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, output_dim),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        h = x.view(x.shape[0], -1)
+        x = self.classifier(h)
+        return x, h
+    
+vgg11_config = [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
+
+vgg13_config = [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512,
+                512, 'M']
+
+vgg16_config = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512,
+                'M', 512, 512, 512, 'M']
+
+vgg19_config = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512,
+                512, 512, 'M', 512, 512, 512, 512, 'M']
+
+
+def get_vgg_layers(config, batch_norm):
+
+    layers = []
+    in_channels = 3
+
+    for c in config:
+        assert c == 'M' or isinstance(c, int)
+        if c == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, c, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(c), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = c
+
+    return nn.Sequential(*layers)
+
+vgg11_layers = get_vgg_layers(vgg11_config, batch_norm=True)
+vgg19_layers = get_vgg_layers(vgg19_config, batch_norm=True)
+
+
+model_urls = {
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+    'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+    'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+}
+
+
+def get_vgg(arch='vgg19_bn', batch_norm=True, label_dim=64, pretrained=True, progress=True):
+    model = VGG(get_vgg_layers(vgg19_config, batch_norm=batch_norm), output_dim=label_dim)
+    
+    if pretrained:
+        pretrained_dict = load_state_dict_from_url(model_urls[arch], progress=progress)
+
+        for key in list(pretrained_dict.keys()):
+            if 'classifier' in key: pretrained_dict.pop(key)
+
+        model.load_state_dict(pretrained_dict, strict=False)
+        print("Initializing Path Weights")
+
+    return model
+
+
+
+
+
+OUTPUT_DIM = 64
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+model = get_vgg().to(device=DEVICE,dtype=torch.float)
+summary(model,(3, 224, 224))
