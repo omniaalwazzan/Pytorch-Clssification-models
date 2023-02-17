a5cdcbdae43526c4c2d5feccf33e53e20380bd9c
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2023-02-17 12:52:34 +0000

    Create plot_transformedImg.py

---
+++ utils/plot_transformedImg.py
@@ -0,0 +1,80 @@
+import random
+from PIL import Image 
+import matplotlib.pyplot as plt
+
+import torch
+from torchvision import transforms
+
+
+import random
+from PIL import Image
+from pathlib import Path
+
+# Setup path to data folder
+data_path = Path(r".\animals")
+image_path = data_path / "cat_pan_dog"
+# Set seed
+random.seed(42) # <- try changing this and see what happens
+
+# 1. Get all image paths (* means "any combination")
+image_path_list = list(image_path.glob("*.jpg"))
+
+# 2. Get random image path
+random_image_path = random.choice(image_path_list)
+
+# 3. Get image class from path name (the image class is the name of the directory where the image is stored)
+image_class = random_image_path.parent.stem
+
+# 4. Open image
+img = Image.open(random_image_path)
+
+# 5. Print metadata
+print(f"Random image path: {random_image_path}")
+print(f"Image height: {img.height}") 
+print(f"Image width: {img.width}")
+
+# Write transform for image
+data_transform = transforms.Compose([
+    # Resize the images to 64x64
+    transforms.Resize(size=(64, 64)),
+    # Flip the images randomly on the horizontal
+    transforms.RandomHorizontalFlip(p=0.5), # p = probability of flip, 0.5 = 50% chance
+    # Turn the image into a torch.Tensor
+    transforms.ToTensor() # this also converts all pixel values from 0 to 255 to be between 0.0 and 1.0 
+])
+
+
+def plot_transformed_images(image_paths, transform, n=3, seed=42):
+    """Plots a series of random images from image_paths.
+
+    Will open n image paths from image_paths, transform them
+    with transform and plot them side by side.
+
+    Args:
+        image_paths (list): List of target image paths. 
+        transform (PyTorch Transforms): Transforms to apply to images.
+        n (int, optional): Number of images to plot. Defaults to 3.
+        seed (int, optional): Random seed for the random generator. Defaults to 42.
+    """
+    random.seed(seed)
+    random_image_paths = random.sample(image_paths, k=n)
+    for image_path in random_image_paths:
+        with Image.open(image_path) as f:
+            fig, ax = plt.subplots(1, 2)
+            ax[0].imshow(f) 
+            ax[0].set_title(f"Original \nSize: {f.size}")
+            ax[0].axis("off")
+
+            # Transform and plot image
+            # Note: permute() will change shape of image to suit matplotlib 
+            # (PyTorch default is [C, H, W] but Matplotlib is [H, W, C])
+            transformed_image = transform(f).permute(1, 2, 0) 
+            ax[1].imshow(transformed_image) 
+            ax[1].set_title(f"Transformed \nSize: {transformed_image.shape}")
+            ax[1].axis("off")
+
+            fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=16)
+
+plot_transformed_images(image_path_list, 
+                        transform=data_transform, 
+                        n=3)
