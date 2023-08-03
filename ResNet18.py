31ff102a89332d959236261e5206c61f49e5693f
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2023-08-04 00:55:57 +0100

    Update ResNet18.py

--- ResNet18.py
+++ ResNet18.py
@@ -1,30 +1,23 @@
 
 import torchvision
 from torchvision import models
-from torchvision.models import convnext_base, ConvNeXt_Base_Weights
+from torchvision.models import convnext_base, ConvNeXt_Base_Weights, ResNet18_Weights
 
 from torch import nn
 import torch
 from torchsummary import summary
-from torchvision.models import ResNet18_Weights
-
-
-
-
+from torchinfo import summary
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 cmmd_gt = 2
 class Res(nn.Module):
     def __init__(self, n_classes=cmmd_gt):
         super().__init__()
-        R18 = models.resnet18(pretrained=True)
+        R18 = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
         feature_extractor = nn.Sequential(*list(R18.children())[:-2])
         self.feature = feature_extractor
         self.calssifier =nn.Sequential(nn.Linear(512 * 16 * 16, 1024),
-                                       #nn.ReLU(True),
-                                       #nn.Dropout(0.25),
                                        nn.Linear(1024, 512),
-                                       #nn.ReLU(True),
                                        nn.Dropout(0.25),
                                        nn.Linear(512, n_classes))
 
@@ -38,7 +31,8 @@ class Res(nn.Module):
         return flatten_featur, x
     
 model =Res()
-summary(model, (3,512,512))
+#summary(model, (3,512,512))
+print(summary(model, (2,3,512,512)))
 img = torch.randn(2,3,512,512)
 fea,out = model(img)
 print(f"shape of feature:{fea.shape}\nshape of output {out.shape}")#
